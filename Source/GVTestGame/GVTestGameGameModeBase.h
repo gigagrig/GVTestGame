9726ff57abe2a9fdbf18688280957012ee93cf58
Grigoriy Volkov <g_volkov@wargaming.net>
2021-01-27 16:01:28 +0300

    Project start

---
+++ Source/GVTestGame/GVTestGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GVTestGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GVTESTGAME_API AGVTestGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
